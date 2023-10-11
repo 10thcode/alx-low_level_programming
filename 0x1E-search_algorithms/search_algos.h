cf931496f431249a4a69143a81b7af33d2bc4a9b
10thcode <attabadukwabena@gmail.com>
2023-10-11 16:44:32 +0000

    Update search_algos.h
    Add function prototype for jump_search()

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,11 @@
 #define HEADER
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_sub_array(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* HEADER */
